a5b7554a9cf5e5d89420473e185b5cd145c55771
HP <HP@DESKTOP-CBG6448>
2020-06-10 18:00:35 +0530

    Refactoring

--- TDD-Calculator/src/main/java/io/myprac/StringCalculator.java
+++ TDD-Calculator/src/main/java/io/myprac/StringCalculator.java
@@ -5,16 +5,21 @@ public class StringCalculator
 	public int Add(String numbers)
 	{
 		if(numbers.isEmpty())
-			{
-				return 0;
-			}
-		else if(numbers.contains(","))
-			{
-				String[] num = numbers.split(",");
-				return Integer.parseInt(num[0]) + Integer.parseInt(num[1]);
-			}
+		{
+			return 0;
+		}
 		
-		return Integer.parseInt(numbers);
+		else if(numbers.length()==1)
+		{
+			return Integer.parseInt(numbers);
+		}
+		
+		else
+		{
+			String[] num = numbers.split(",");
+			return Integer.parseInt(num[0]) + Integer.parseInt(num[1]);
+		}
+
 	}
 	
 }
