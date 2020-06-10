33247bc55b9e22bf1f0a6f2476d45c683918bf93
HP <HP@DESKTOP-CBG6448>
2020-06-10 17:38:28 +0530

    Step 1.2 (Test Passed) and refactoring

--- TDD-Calculator/src/main/java/io/myprac/StringCalculator.java
+++ TDD-Calculator/src/main/java/io/myprac/StringCalculator.java
@@ -4,7 +4,11 @@ public class StringCalculator
 {
 	public int Add(String numbers)
 	{
-		return 0;
+		if(numbers.isEmpty())
+			{
+				return 0;
+			}
+		return Integer.parseInt(numbers);
 	}
 	
 }
--- TDD-Calculator/src/test/java/io/myprac/StringCalculatorTest.java
+++ TDD-Calculator/src/test/java/io/myprac/StringCalculatorTest.java
@@ -2,22 +2,28 @@ package io.myprac;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class StringCalculatorTest 
 {
+	StringCalculator cal;
 
+	@BeforeEach
+	void init()
+	{
+		cal = new StringCalculator();
+	}
+	
 	@Test
 	void whenEmptyString()
 	{
-		StringCalculator cal = new StringCalculator();
 		assertEquals(0, cal.Add(""), "When Empty should return zero");
 	}
 	
 	@Test
 	void whenSingleString()
 	{
-		StringCalculator cal = new StringCalculator();
 		assertEquals(1, cal.Add("1"), "When single string, should return the same string");
 	}
 }
