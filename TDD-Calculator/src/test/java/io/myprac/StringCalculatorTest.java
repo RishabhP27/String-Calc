9247874e65feef4d0da88a7d94f94f084be011a9
HP <HP@DESKTOP-CBG6448>
2020-06-10 17:26:24 +0530

    Step 1.2 (Failed Test)

--- TDD-Calculator/src/test/java/io/myprac/StringCalculatorTest.java
+++ TDD-Calculator/src/test/java/io/myprac/StringCalculatorTest.java
@@ -14,4 +14,10 @@ class StringCalculatorTest
 		assertEquals(0, cal.Add(""), "When Empty should return zero");
 	}
 	
+	@Test
+	void whenSingleString()
+	{
+		StringCalculator cal = new StringCalculator();
+		assertEquals(1, cal.Add("1"), "When single string, should return the same string");
+	}
 }
